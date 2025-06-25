a4ec866e1a58cd6682e25a8413ac5c0dc5bbd805
Meron T. Gebru <meronteklehaymanotgebru@gmail.com>
2025-06-25 14:51:29 +0300

    Added some chnages

--- tujijenge/tujijenge/settings.py
+++ tujijenge/tujijenge/settings.py
@@ -88,11 +88,6 @@ DATABASES = {
     }
 }
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> origin/develop
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
