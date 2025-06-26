045b71f547ca3c49a3dbf08f73692dbfe98e9d4f
studen <arsemaagebremichael@gmail.com>
2025-06-26 09:37:35 +0300

    Build unified API for both users

--- tujijenge/tujijenge/settings.py
+++ tujijenge/tujijenge/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d!&$)$))=z5)q*_w$x2dx9kuh+=9brnqr!74llyy+123#eo7yl'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,10 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'communities',
-    'payments',
     'users',
-    'stock'
+    'rest_framework',
+    'users_api',
 ]
 
 MIDDLEWARE = [
@@ -78,12 +77,8 @@ WSGI_APPLICATION = 'tujijenge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'HOST':'aws-0-us-east-2.pooler.supabase.com',
-        'PORT' : '5432',
-        'USER':'postgres.creibsbzmowhdoweorps',
-        'PASSWORD' : 'funchixkre#'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
