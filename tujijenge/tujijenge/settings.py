bbbc4131ae2ac567f91dc81b7eac91661b4e974e
karen <karenwanngugi@gmail.com>
2025-06-26 08:27:37 +0300

    changed url path for api

--- tujijenge/tujijenge/settings.py
+++ tujijenge/tujijenge/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'comm_api',
     'communities',
     'payments',
     'users',
--- tujijenge/tujijenge/urls.py
+++ tujijenge/tujijenge/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('comm_api/',include('comm_api.urls'))
+    path('api/',include('comm_api.urls'))
 ]
