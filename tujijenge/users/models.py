23cdc99696ffe4ae829d51e5ab4b459ef82d5a1e
Meron T. Gebru <meronteklehaymanotgebru@gmail.com>
2025-06-27 11:46:41 +0300

    Revert last commit to rename requirements.txt Revert
    This reverts commit 018e12b1efb9987b076f318e71f6500eeb127c09.

--- tujijenge/users/models.py
+++ tujijenge/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 class Mamamboga(models.Model):
     id = models.CharField(max_length=5, primary_key=True)
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
     phone_number = models.CharField(max_length=20, unique=True)
     pin = models.CharField(max_length=4)
     latitude = models.FloatField(null=True, blank=True)
@@ -24,9 +24,3 @@ class Stakeholder(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
-
-
-
-
-
