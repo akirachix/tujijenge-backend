92698a4e789bb2eb2b4cb02bd6d0a36f53313b81
Meron T. Gebru <meronteklehaymanotgebru@gmail.com>
2025-06-27 11:33:28 +0300

    Renamed requirements file, added migrations

--- tujijenge/stock_api/serializers.py
+++ tujijenge/stock_api/serializers.py
@@ -1,17 +1,6 @@
 from rest_framework import serializers
 from stock.models import  Product, Stock
-# from users.models import Mamamboga
 
-
-# class MamambogaSerializer(serializers.ModelSerializer):
-#     mamamboga_name = serializers.SerializerMethodField()
-
-    # class Meta:
-    #     model = Mamamboga
-    #     fields = ['id', 'mamamboga_name']
-
-    # def get_mamamboga_name(self, obj):
-    #     return f"{obj.first_name} {obj.last_name or ''}".strip()
 
 class ProductSerializer(serializers.ModelSerializer):
 
@@ -23,11 +12,6 @@ class ProductSerializer(serializers.ModelSerializer):
         ]
 
 class StockSerializer(serializers.ModelSerializer):
-    # mamamboga = MamambogaSerializer(read_only=True)
-    # mamamboga_id = serializers.PrimaryKeyRelatedField(
-    #     queryset=Mamamboga.objects.all(), source='mamamboga', write_only=True, allow_null=True
-    # )
-
     class Meta:
         model = Stock
         fields = [
