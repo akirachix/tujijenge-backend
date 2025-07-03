16f541eefbaf47e0dde00ab04cafdfdac4fb4e19
A <Arsema.arsemaagebremichael@gmail.com>
2025-07-03 14:03:29 +0300

    Remove .env file

--- tujijenge/users/models.py
+++ tujijenge/users/models.py
@@ -23,7 +23,6 @@ class Stakeholder(models.Model):
     last_name = models.CharField(max_length=50)
     stakeholder_email = models.EmailField(max_length=255, unique=True)
     password_hash = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-
+    created_at = models.DateTimeField(auto_now_add=True)   
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
