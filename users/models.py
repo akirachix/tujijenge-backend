f8ae1c9700ebce491a91baa1ef66bea252af453d
Arsema <arsemaagebremichael@gmail.com>
2025-07-10 12:02:58 +0300

    Add geocoging to tujijenge app

--- users/models.py
+++ users/models.py
@@ -15,6 +15,7 @@ class Mamamboga(models.Model):
     is_active = models.BooleanField(default=True)
     deactivation_date = models.DateTimeField(null=True, blank=True)
     certified_status = models.CharField(max_length=15, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
