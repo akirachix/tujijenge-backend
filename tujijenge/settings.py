2f65b7b0ddc103903d55296d16782f84a6149c25
Arsema <arsemaagebremichael@gmail.com>
2025-07-21 10:39:46 +0300

    Add authentication and authorization

--- tujijenge/settings.py
+++ tujijenge/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'communities',
     'rest_framework',
+    "rest_framework.authtoken",
     'orders',
      "corsheaders",
     'users',
     'stock',
     'api',
+    'cart',
 ]
 
 MIDDLEWARE = [
