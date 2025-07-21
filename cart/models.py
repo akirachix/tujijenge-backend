4727b11e2a03304e09c41152a4d379ab1d53151a
Arsema <arsemaagebremichael@gmail.com>
2025-07-21 11:37:05 +0300

    Add authentication and authorization

--- cart/models.py
+++ cart/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from decimal import Decimal
 from django.core.validators import MinValueValidator
 from stock.models import Product
-from stock.models import Product
 
 class Cart(models.Model):
     mamamboga = models.ForeignKey(
